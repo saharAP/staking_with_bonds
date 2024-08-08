8aaaf495e9ae4402a9e6cb79fa22f280b1be3cf6
saharAP <sahar.aplo@gmail.com>
2024-08-08 11:34:35 -0400

    Staker contract added

---
+++ src/Staker.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Staker is Pausable, Ownable {
+
+    
+}
