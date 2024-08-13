3172b5c7145b8bd5a36f95968eaa2008c054190d
saharAP <sahar.aplo@gmail.com>
2024-08-12 23:00:58 -0400

    St1wToken token added

---
+++ src/tokens/St1wToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract St1wToken is ERC20, Ownable {
+
+constructor() ERC20("Staked 1 Week Token", "st-1w") Ownable(msg.sender){
+    
+}
+function mint(address account, uint256 value)public onlyOwner{
+    _mint(account, value);
+}
+function burn(address account, uint256 value)public onlyOwner{
+    _burn(account, value);
+}
+
+}
